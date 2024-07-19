c6953a4c2d9bc228f5dfafcba765f7b7563680c3
Kang Jin Yong <kangjinyong@gmail.com>
2024-07-19 17:10:14 +0800

    revert name

--- TimerTrigger1.cs
+++ TimerTrigger1.cs
@@ -4,24 +4,24 @@ using Microsoft.Extensions.Logging;
 
 namespace sampleapp.function
 {
-    public class TimerTriggerTest
+    public class TimerTrigger1
     {
         private readonly ILogger _logger;
 
-        public TimerTriggerTest(ILoggerFactory loggerFactory)
+        public TimerTrigger1(ILoggerFactory loggerFactory)
         {
-            _logger = loggerFactory.CreateLogger<TimerTriggerTest>();
+            _logger = loggerFactory.CreateLogger<TimerTrigger1>();
         }
 
-        [Function("TimerTriggerTest")]
+        [Function("TimerTrigger1")]
         public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
         {
             _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
             
-            // if (myTimer.ScheduleStatus is not null)
-            // {
-            //     _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
-            // }
+            if (myTimer.ScheduleStatus is not null)
+            {
+                _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+            }
         }
     }
 }
