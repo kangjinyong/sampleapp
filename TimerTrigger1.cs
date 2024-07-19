1ee8198e17eceadf4067f109de791ea3532dc6b5
Kang Jin Yong <kangjinyong@gmail.com>
2024-07-19 16:26:34 +0800

    test rename function

--- TimerTrigger1.cs
+++ TimerTrigger1.cs
@@ -13,15 +13,15 @@ namespace sampleapp.function
             _logger = loggerFactory.CreateLogger<TimerTrigger1>();
         }
 
-        [Function("TimerTrigger1")]
+        [Function("TimerTriggerTest")]
         public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
         {
             _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
             
-            if (myTimer.ScheduleStatus is not null)
-            {
-                _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
-            }
+            // if (myTimer.ScheduleStatus is not null)
+            // {
+            //     _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+            // }
         }
     }
 }
