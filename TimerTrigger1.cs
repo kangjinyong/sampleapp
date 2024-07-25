a9ce5f622110b1617f5aee7457840e7a8d0ac837
Kang Jin Yong <kangjinyong@gmail.com>
2024-07-25 14:55:04 +0800

    add datetimestamp and checksum file

--- TimerTrigger1.cs
+++ TimerTrigger1.cs
@@ -60,11 +60,14 @@ namespace sampleapp.function
         }
 
         private async Task CopyFiles(BlobContainerClient containerFrom, BlobContainerClient containerTo) {
+            string dateTimeStamp = (new DateTimeOffset(new DateTime(), new TimeSpan(8,0,0))).ToString("yyyyMMddhhmmssfff");
+            List<string> checksums = new List<string>();
+            string checksumFilename = string.Format("{0}/Checksum_{0}.csv", dateTimeStamp);
             await foreach (var blobItem in containerFrom.GetBlobsAsync())
             {
                 string fromName = blobItem.Name;
                 string[] fromNameArray = fromName.Split("/");
-                string toName = string.Format("{0}.csv", fromNameArray[fromNameArray.Length-1].Split(".")[0]);
+                string toName = string.Format("{0}/{1}_{0}.csv", dateTimeStamp, fromNameArray[fromNameArray.Length-1].Split(".")[0]);
                 BlobClient fromClient = containerFrom.GetBlobClient(fromName);
                 BlobClient toClient = containerTo.GetBlobClient(toName);
                 BlobDownloadStreamingResult stream = (await fromClient.DownloadStreamingAsync()).Value;
@@ -75,22 +78,27 @@ namespace sampleapp.function
                         await toClient.UploadAsync(unzippedStream, overwrite: true);
                         _logger.LogInformation(string.Format("{0} successfully unzipped and copied to {1}.", fromName, toName));
 
-                        using (SHA512 sha512 = SHA512.Create())
+                        using (SHA256 sha256 = SHA256.Create())
                         {
-                            byte[] hash = await Task.Run(() => sha512.ComputeHash(unzippedStream));
+                            byte[] hash = sha256.ComputeHash(unzippedStream);
                             StringBuilder sb = new StringBuilder();
                             foreach (byte b in hash)
                             {
                                 sb.Append(b.ToString("x2"));
                             }
-                            _logger.LogInformation(string.Format("Checksum: {0}", sb.ToString()));
+                            checksums.Add(string.Format("{0} ~ {1}", toName, sb.ToString()));
                         }
                     }
                 }     
-                catch (Exception ex) {
+                catch {
                     _logger.LogInformation(string.Format("{0} cannot be unzipped and copied.", fromName));
-                    _logger.LogInformation(string.Format("Error detected: {0}", ex.Message));
-                }        
+                } 
+            }
+            BlobClient checksumClient = containerTo.GetBlobClient(checksumFilename);
+            byte[] byteArray = Encoding.UTF8.GetBytes(string.Join(Environment.NewLine, checksums));
+            using (MemoryStream stream = new MemoryStream(byteArray))
+            {
+                await checksumClient.UploadAsync(stream, overwrite: true);
             }
         }
     }
